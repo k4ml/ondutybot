d8628ae4d8fe96b92cac1c985d60d92da53d3587
Kamal Mustafa <kamal.mustafa@gmail.com>
2018-10-17 18:51:37 +0800

    initial

---
+++ go.mod
@@ -0,0 +1,8 @@
+module ondutybot
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.0.0-20181011115005-2adea7c8be71
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"log"
+	"fmt"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		fmt.Println("New members", update.Message.NewChatMembers)
+		if update.Message.NewChatMembers != nil {
+			for _, newUser := range *update.Message.NewChatMembers {
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Welcome %s", newUser.FirstName)))
+			}
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
