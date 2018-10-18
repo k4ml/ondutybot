c4ab13ca8fbeeaf4a6e28350bdc4b43c255b8637
Kamal Mustafa <kamal.mustafa@gmail.com>
2018-10-18 08:14:07 +0800

    initial question and kick logic

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+func RemoveIndex(s []tgbotapi.User, index int) []tgbotapi.User {
+	return append(s[:index], s[index+1:]...)
+}
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
 	if err != nil {
@@ -23,6 +27,10 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
+	pendingNewUsers := make([]tgbotapi.User, 0)
+	question := "Siapa PM kita yang ke 4 dan 7?"
+	choices := "tar/dr.m, pak lah & dsn, dr.m & tun m"
+	answer := "dr. m & tun m"
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
@@ -31,15 +39,35 @@ func main() {
 		fmt.Println("New members", update.Message.NewChatMembers)
 		if update.Message.NewChatMembers != nil {
 			for _, newUser := range *update.Message.NewChatMembers {
-				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Welcome %s", newUser.FirstName)))
+				pendingNewUsers = append(pendingNewUsers, newUser)
+				message := "Welcome %s. Sila jawab dgn Reply: %s?[%s]"
+				message = fmt.Sprintf(message, newUser.FirstName, question, choices)
+				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, message))
 			}
 		}
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		fmt.Println("Pending users", pendingNewUsers)
+		for index, pendingUser := range pendingNewUsers {
+			if *update.Message.From == pendingUser {
+				if update.Message.Text != answer {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Wrong answer. You'll be removed."))
+					chatMemberConfig := tgbotapi.ChatMemberConfig{ChatID: update.Message.Chat.ID, UserID: update.Message.From.ID}
+					kickChatMemberConfig := tgbotapi.KickChatMemberConfig{ChatMemberConfig:chatMemberConfig}
+					fmt.Println(kickChatMemberConfig)
+					bot.KickChatMember(kickChatMemberConfig)
+					pendingNewUsers = RemoveIndex(pendingNewUsers, index)
+				} else {
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Correct. You're verified"))
+				}
+			}
+		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		//fmt.Println(tgbotapi.ChatConfig(*update.Message.Chat))
 
-		bot.Send(msg)
+		//msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		//msg.ReplyToMessageID = update.Message.MessageID
+
+		//bot.Send(msg)
 	}
 }
